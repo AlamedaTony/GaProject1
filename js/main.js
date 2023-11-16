8d90ed204fdd7d3615423b659e5f1568e1defb2f
Tony Alameda <alamedatony1@gmail.com>
2023-11-15 16:23:06 -0800

    display cards face down on board

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-	/*----- constants -----*/
+    /*Set all cards face down across board
+    when clicked the 1st card is flipped face up, check to see if the card is already face up
+    if the card is not already face up, flip the 2nd card face up.
+    if 1st card and 2nd card are a match stay flipped face up.
+    if 1st card and 2nd card are not a match flipp face down.*/
+    
+    
+    
+    /*----- constants -----*/
     const suits = ["s", "c", "d", "h"];
     const ranks = ["02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K", "A" ];
 
@@ -8,16 +16,20 @@
 	/*----- state variables -----*/
     let board;
     let winner;
-    let cards;
+    let currentCard;
     let shuffledDeck;
 
 	/*----- cached elements  -----*/
     const playAgainBtn = document.getElementById("play again");
     const shuffledContainer = document.getElementById("shuffled-deck-container");
-
+    //console.log(shuffledContainer);
+    
+    
+    
+    //console.log(shuffledDeck);
 	/*----- event listeners -----*/
     document.querySelector("button").addEventListener("click", onPlayAgainClick);
-    document.querySelector(".card-container").addEventListener("click", onCardClick);
+    
 	/*----- functions -----*/
     init();
 
@@ -29,8 +41,7 @@
             [],
             [],
         ];
-        //first create a shuffled array of card objects and assign to card state
-        //then, take each card object push each row
+        
     }
 
     function onPlayAgainClick() {
@@ -45,14 +56,15 @@
 
     }
 
-    function flipCard() {
-
-    }
-
-    function onCardClick() {
-        //if a card is already visable(face value showing), do nothing
-        //if ()
+    function onCardClick(evt) {
         //flip card over showing the "face" value
+        evt.target.classList.remove("back");
+        /*When a card is clicked the back class is removed
+        */
+       
+       
+
+        //if a card is already visable(face value showing), do nothing
         //if card already flipped and clicked card doesn't match, flip back over to "back" value
         //if card is already flipped and cicked card does match, stay flipped
         //if all cards are clicked, needs to render winner message
@@ -63,19 +75,15 @@
         const tempDeck = [...originalDeck];
         const newShuffledDeck = [];
         while (tempDeck.length) {
-
             //need to know if a card is visable(face value is showing) or not
 
             //need to know if it is matched
-
             const rndIdx = Math.floor(Math.random() * tempDeck.length);
             tempDeck[rndIdx].visable = false;
             tempDeck[rndIdx].matched = false;
-
             newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-
         }
-        console.log(newShuffledDeck);
+        
         return newShuffledDeck;
     }
 
@@ -103,8 +111,15 @@
         //build the cards as a string of HTML
         let cardsHtml = "";
         deck.forEach(function(card) {
-            cardsHtml += `<div class="card ${card.face}"></div>`;
+            cardsHtml += `<div class="card back ${card.face}"></div>`;
+            
         });
         container.innerHTML = cardsHtml;
+        const cards = document.querySelectorAll(".card");
+        //console.log(cards);
+            
+        cards.forEach(function(card) {
+             card.addEventListener("click", onCardClick)
+           })
     }
     renderNewShuffledDeck();
